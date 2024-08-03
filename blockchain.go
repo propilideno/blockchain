f7bb5c29a355d8cfa916a6228856637037db8428
lucas <lucas@propi.dev>
2024-08-03 16:07:35 -0300

    feat: adding memory pool and separating miners of users

--- blockchain.go
+++ blockchain.go
@@ -11,27 +11,39 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// Transaction represents a blockchain transaction
+type Transaction struct {
+	From   string  `json:"from"`
+	To     string  `json:"to"`
+	Amount float64 `json:"amount"`
+}
+
+// Block represents each 'item' in the blockchain
 type Block struct {
-	Data         map[string]interface{}
+	Transactions []Transaction
 	Hash         string
 	PreviousHash string
 	Timestamp    time.Time
 	PoW          int
 }
 
+// Blockchain represents the entire chain
 type Blockchain struct {
 	GenesisBlock Block
 	Chain        []Block
+	MemoryPool   []Transaction
 	Difficulty   int
 }
 
+// calculateHash calculates the hash of a block
 func (b Block) calculateHash() string {
-	data, _ := json.Marshal(b.Data)
+	data, _ := json.Marshal(b.Transactions)
 	blockData := b.PreviousHash + string(data) + b.Timestamp.String() + strconv.Itoa(b.PoW)
 	blockHash := sha256.Sum256([]byte(blockData))
 	return fmt.Sprintf("%x", blockHash)
 }
 
+// mine mines a block
 func (b *Block) mine(difficulty int) {
 	for !strings.HasPrefix(b.Hash, strings.Repeat("0", difficulty)) {
 		b.PoW++
@@ -39,6 +51,7 @@ func (b *Block) mine(difficulty int) {
 	}
 }
 
+// CreateBlockchain creates a new blockchain with a genesis block
 func CreateBlockchain(difficulty int) Blockchain {
 	genesisBlock := Block{
 		Hash:      "0",
@@ -51,17 +64,33 @@ func CreateBlockchain(difficulty int) Blockchain {
 	}
 }
 
-func (b *Blockchain) addBlock(data map[string]interface{}) {
+// addTransaction adds a new transaction to the memory pool
+func (b *Blockchain) addTransaction(transaction Transaction) {
+	b.MemoryPool = append(b.MemoryPool, transaction)
+}
+
+// mine mines a new block containing transactions from the memory pool
+func (b *Blockchain) mine() Block {
+	if len(b.MemoryPool) == 0 {
+		return Block{}
+	}
+
 	lastBlock := b.Chain[len(b.Chain)-1]
 	newBlock := Block{
-		Data:         data,
+		Transactions: b.MemoryPool,
 		PreviousHash: lastBlock.Hash,
 		Timestamp:    time.Now(),
 	}
 	newBlock.mine(b.Difficulty)
 	b.Chain = append(b.Chain, newBlock)
+
+	// Clear the memory pool
+	b.MemoryPool = nil
+
+	return newBlock
 }
 
+// isValid checks if the blockchain is valid
 func (b Blockchain) isValid() bool {
 	for i := range b.Chain[1:] {
 		previousBlock := b.Chain[i]
@@ -73,6 +102,7 @@ func (b Blockchain) isValid() bool {
 	return true
 }
 
+// main sets up the server and routes
 func main() {
 	app := fiber.New()
 
@@ -85,45 +115,43 @@ func main() {
 		return c.Next()
 	})
 
+	// Mine a new block
 	app.Get("/mine", func(c *fiber.Ctx) error {
 		blockchain := c.Locals("blockchain").(*Blockchain)
+		block := blockchain.mine()
 
-		// Mine a new block with a reward transaction
-		transaction := map[string]interface{}{
-			"from":   "0",
-			"to":     "miner-address",
-			"amount": 1,
+		if block.Hash == "" {
+			return c.Status(fiber.StatusForbidden).SendString("No transactions to mine")
 		}
-		blockchain.addBlock(transaction)
 
 		response := fiber.Map{
 			"message": "New Block Forged",
 			"index":   len(blockchain.Chain) - 1,
+			"block":   block,
 		}
 		return c.Status(fiber.StatusOK).JSON(response)
 	})
 
+	// Add a new transaction
 	app.Post("/transactions/new", func(c *fiber.Ctx) error {
-		var transaction map[string]interface{}
+		var transaction Transaction
 		if err := c.BodyParser(&transaction); err != nil {
 			return c.Status(fiber.StatusBadRequest).SendString("Invalid input")
 		}
-
-		// Ensure transaction data is correct
-		if transaction["from"] == nil || transaction["to"] == nil || transaction["amount"] == nil {
+		if transaction.From == "" || transaction.To == "" || transaction.Amount <= 0 {
 			return c.Status(fiber.StatusBadRequest).SendString("Missing transaction data")
 		}
 
 		blockchain := c.Locals("blockchain").(*Blockchain)
-		blockchain.addBlock(transaction)
+		blockchain.addTransaction(transaction)
 
-		response := fiber.Map{"message": "Transaction added to the blockchain"}
+		response := fiber.Map{"message": "Transaction added to the memory pool"}
 		return c.Status(fiber.StatusCreated).JSON(response)
 	})
 
+	// Get the full blockchain
 	app.Get("/chain", func(c *fiber.Ctx) error {
 		blockchain := c.Locals("blockchain").(*Blockchain)
-
 		response := fiber.Map{
 			"chain":  blockchain.Chain,
 			"length": len(blockchain.Chain),
